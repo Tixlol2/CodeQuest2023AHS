7608263127620c85364c9d08a69e6ec9e34072db
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-10-11 13:15:21 -0500

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)){
+            int testCases = Integer.parseInt(input.nextLine());
+        
+        for(int testcase = 0; testcase < testCases; testcase++) {
+            String input1 = input.nextLine();
+            String input2[] = input1.split(":");
+            double speed = Double.parseDouble(input2[0]);
+            double dist = Double.parseDouble(input2[1]);
+            
+            if (speed >= dist){
+              
+              System.out.println("SWERVE");
+              
+              
+            }
+            
+            else if ((speed * 5) >= dist){
+              
+              System.out.println("BRAKE");
+              
+              
+            }
+            
+            else{
+              
+              System.out.println("SAFE");
+              
+              
+            }
+            
+            
+            
+                }
+            }
+        }
+    }
