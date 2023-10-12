b53aacf477a7cb07e8a46083dfd39df1eef620ab
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-10-12 11:39:19 -0500

    Create the-bottom-line.java

---
+++ the-bottom-line.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)){
+            int testCases = Integer.parseInt(input.nextLine());
+
+            for(int testcase = 0; testcase < testCases; testcase++) {
+
+                String first = input.nextLine();
+
+                int cc = Integer.parseInt(first.split(" ")[0]);
+
+                int lbl = Integer.parseInt(first.split(" ")[1]);
+
+                if (cc == lbl){
+
+                    System.out.println("Cassowary Craft and Lead Balloons Ltd solds the same number of aircraft");
+
+
+                } else if (cc > lbl) {
+
+                    System.out.println("Cassowary Craft sold " + (cc - lbl) + " more aircraft");
+
+
+                } else if (lbl > cc) {
+
+                    System.out.println("Lead Balloons Ltd sold " + (lbl - cc) + " more aircraft");
+
+
+                }
+
+
+            }
+        }
+    }
+}
