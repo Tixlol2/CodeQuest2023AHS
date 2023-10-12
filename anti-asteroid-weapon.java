27c5fe7e6edb068f447ec7817ecdee28e05cfbf5
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-10-12 11:31:11 -0500

    Create anti-asteroid-weapon.java

---
+++ anti-asteroid-weapon.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)){
+            int testCases = Integer.parseInt(input.nextLine());
+
+            for(int testcase = 0; testcase < testCases; testcase++) {
+
+                List<String> asteroids_list = new ArrayList<>();
+                
+                List<Double> asteroid_dist = new ArrayList<>();
+
+                int asteroids = input.nextInt();
+
+                String first = input.nextLine();
+
+                int i = 0;
+
+                while ( i < asteroids){
+
+                    asteroids_list.add(input.nextLine());
+
+                    i++;
+                }
+
+                for (String asteroid : asteroids_list){
+
+                    
+
+
+                }
+
+
+            }
+        }
+    }
+}
