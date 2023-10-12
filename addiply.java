4775ff309d6a87e7bd5bcc42ccf9db3af9f51592
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-10-11 21:27:10 -0500

    Create addiply.java

---
+++ addiply.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)){
+            int testCases = Integer.parseInt(input.nextLine());
+        
+        for(int testcase = 0; testcase < testCases; testcase++) {
+            
+            String first = input.nextLine();
+            String second[] = first.split(" ");
+            int num1 = Integer.parseInt(second[0]);
+            int num2 = Integer.parseInt(second[1]);
+            
+            int sum = num1 + num2;
+            int product = num1 * num2;
+            
+            
+            
+            System.out.println(Integer.toString(sum) + " " + Integer.toString(product));
+            
+            
+            
+                }
+            }
+        }
+    }
