217fae3a5759a42ebf90d08e59ef21672f287136
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-10-11 21:39:45 -0500

    Create animal-farm.java

---
+++ animal-farm.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)){
+            int testCases = Integer.parseInt(input.nextLine());
+
+            for(int testcase = 0; testcase < testCases; testcase++) {
+
+                String first = input.nextLine();
+                String first_list[] = first.split(" ");
+                int turkey = Integer.parseInt(first_list[0]);
+                int goats = Integer.parseInt(first_list[1]);
+                int horses = Integer.parseInt(first_list[2]);
+
+                int sum = (turkey*2) + (goats * 4) + (horses * 4);
+                
+                System.out.println(sum);
+
+            }
+        }
+    }
+}
