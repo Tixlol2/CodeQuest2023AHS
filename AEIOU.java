e960f2e7126a416cc71eeabff0aa5901b879c1ba
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-10-11 21:59:30 -0500

    Update AEIOU.java

--- AEIOU.java
+++ AEIOU.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)){
+            int testCases = Integer.parseInt(input.nextLine());
 
+            for(int testcase = 0; testcase < testCases; testcase++) {
+
+                String sentence = input.nextLine();
+
+                String letter_list[] = sentence.split("");
+
+                int total = 0;
+
+                for (String letter : letter_list) {
+
+                    if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u") {
+
+                        total = (total + 1);
+
+
+                    }
+
+
+                }
+
+                System.out.println(total);
+
+
+            }
+        }
+    }
+}
